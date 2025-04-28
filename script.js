f5721d4cb08ea0b8e3687e8d1d7ca77af375dcbb
mina <minjjew@gmail.com>
2025-04-28 12:53:15 +0300

    new

--- script.js
+++ script.js
@@ -1,10 +1,69 @@
-var name = "Amina"
-var a = 7
-var b 
+var orders = [
+    {
+      id: 1,
+      customer: "Jhon",
+      items: [
+         { name: "Ноутбук", price: 50000, quantity: 1 },
+  
+         { name: "Мышь", price: 1500, quantity: 2 }
+     ]
+    },
+   {
+    id: 2,
+    customer: "Alan",
+    items: [
+        { name: "Телефон", price: 30000, quantity: 1 },
+  
+        { name: "Чехол", price: 1000, quantity: 3 }
+       ]
+   },
+    {
+    id: 3,
+    customer: "Jane",
+    items: [
+        { name: "Клавиатура", price: 2500, quantity: 1 },
+        { name: "Монитор", price: 12000, quantity: 1 }
+      ]
+   }  
+  ];
+var orderSums = 0;
+var highestOrder = 0;
+var highestOrderCustomer = '';
+var productCounts = {};
+var popularProduct = '';
+var maxQuantity = 0;
 
-console.log(b);
-console.log(false);
-console.log(true);
-console.log("a" * 7);
-console.log(typeof(a));
-console.log(typeof(name));
+for (var i = 0; i < orders.length; i++) {
+  var order = orders[i];
+  var sum = 0;
+  for (var j = 0; j < order.items.length; j++) {
+    var item = order.items[j];
+    sum = sum + item.price * item.quantity;
+
+    if(productCounts[item.name]){
+        productCounts[item.name] += item.quantity
+    }else {
+        productCounts[item.name] = item.quantity
+    }
+  }
+  console.log(order.customer + " " + sum);
+  orderSums = orderSums + sum; 
+
+  if (sum > highestOrder){
+    highestOrder = sum
+    highestOrderCustomer = order.customer
+  }
+}
+console.log("Общий доход: " + orderSums);
+console.log("Самый дорогой заказ сделал- " + highestOrderCustomer +" " + highestOrder);
+console.log("Проданные товары и их количество:");
+for (var product in productCounts) {
+  console.log(product + ": " + productCounts[product] + " шт.");
+}
+for (var product in productCounts) {
+    if (productCounts[product] > maxQuantity) {
+      maxQuantity = productCounts[product];
+      popularProduct = product;
+    }
+  }
+  console.log("Самый популярный товар: " + popularProduct);

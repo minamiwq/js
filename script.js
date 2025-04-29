104a6648d1158211b636f4f4ea41277df7063c41
mina <minjjew@gmail.com>
2025-04-29 21:21:42 +0300

    new code in js function

--- script.js
+++ script.js
@@ -1,69 +1,82 @@
-var orders = [
-    {
-      id: 1,
-      customer: "Jhon",
-      items: [
-         { name: "Ноутбук", price: 50000, quantity: 1 },
+// var orders = [
+//     {
+//       id: 1,
+//       customer: "Jhon",
+//       items: [
+//          { name: "Ноутбук", price: 50000, quantity: 1 },
   
-         { name: "Мышь", price: 1500, quantity: 2 }
-     ]
-    },
-   {
-    id: 2,
-    customer: "Alan",
-    items: [
-        { name: "Телефон", price: 30000, quantity: 1 },
+//          { name: "Мышь", price: 1500, quantity: 2 }
+//      ]
+//     },
+//    {
+//     id: 2,
+//     customer: "Alan",
+//     items: [
+//         { name: "Телефон", price: 30000, quantity: 1 },
   
-        { name: "Чехол", price: 1000, quantity: 3 }
-       ]
-   },
-    {
-    id: 3,
-    customer: "Jane",
-    items: [
-        { name: "Клавиатура", price: 2500, quantity: 1 },
-        { name: "Монитор", price: 12000, quantity: 1 }
-      ]
-   }  
-  ];
-var orderSums = 0;
-var highestOrder = 0;
-var highestOrderCustomer = '';
-var productCounts = {};
-var popularProduct = '';
-var maxQuantity = 0;
+//         { name: "Чехол", price: 1000, quantity: 3 }
+//        ]
+//    },
+//     {
+//     id: 3,
+//     customer: "Jane",
+//     items: [
+//         { name: "Клавиатура", price: 2500, quantity: 1 },
+//         { name: "Монитор", price: 12000, quantity: 1 }
+//       ]
+//    }  
+//   ];
+// var orderSums = 0;
+// var highestOrder = 0;
+// var highestOrderCustomer = '';
+// var productCounts = {};
+// var popularProduct = '';
+// var maxQuantity = 0;
 
-for (var i = 0; i < orders.length; i++) {
-  var order = orders[i];
-  var sum = 0;
-  for (var j = 0; j < order.items.length; j++) {
-    var item = order.items[j];
-    sum = sum + item.price * item.quantity;
+// for (var i = 0; i < orders.length; i++) {
+//   var order = orders[i];
+//   var sum = 0;
+//   for (var j = 0; j < order.items.length; j++) {
+//     var item = order.items[j];
+//     sum += item.price * item.quantity;
 
-    if(productCounts[item.name]){
-        productCounts[item.name] += item.quantity
-    }else {
-        productCounts[item.name] = item.quantity
-    }
+//     if(productCounts[item.name]){
+//         productCounts[item.name] += item.quantity
+//     }else {
+//         productCounts[item.name] = item.quantity
+//     }
+//   }
+//   console.log(order.customer + " " + sum);
+//   orderSums = orderSums + sum; 
+
+//   if (sum > highestOrder){
+//     highestOrder = sum
+//     highestOrderCustomer = order.customer
+//   }
+// }
+// console.log("Общий доход: " + orderSums);
+// console.log("Дорогой заказ сделал- " + highestOrderCustomer +" " + highestOrder);
+// console.log("Проданные товары, количество:");
+// for (var product in productCounts) {
+//   console.log(product + ": " + productCounts[product] + " шт.");
+// }
+// for (var product in productCounts) {
+//     if (productCounts[product] > maxQuantity) {
+//       maxQuantity = productCounts[product];
+//       popularProduct = product;
+//     }
+//   }
+//   console.log("Популярный товар: " + popularProduct);
+
+function MaxNumber(array) {
+  var max = array[0];
+  for (var i = 0; i < array.length; i++) {
+      if(array[i] > max){
+          var max = array[i];
+      }
   }
-  console.log(order.customer + " " + sum);
-  orderSums = orderSums + sum; 
-
-  if (sum > highestOrder){
-    highestOrder = sum
-    highestOrderCustomer = order.customer
-  }
+  return max;
 }
-console.log("Общий доход: " + orderSums);
-console.log("Самый дорогой заказ сделал- " + highestOrderCustomer +" " + highestOrder);
-console.log("Проданные товары и их количество:");
-for (var product in productCounts) {
-  console.log(product + ": " + productCounts[product] + " шт.");
-}
-for (var product in productCounts) {
-    if (productCounts[product] > maxQuantity) {
-      maxQuantity = productCounts[product];
-      popularProduct = product;
-    }
-  }
-  console.log("Самый популярный товар: " + popularProduct);
+console.log("Максимальный [28,45,24,12,56,53,73]: ",MaxNumber([28,45,24,12,56,53,73]));
+console.log("Максимальный [10,-10,-1,12,3]: ",MaxNumber([10,-10,-1,12,3]));
+console.log("Максимальный [-4,-6,-43,-1,0]: ",MaxNumber([-4,-6,-43,-1,0]));
